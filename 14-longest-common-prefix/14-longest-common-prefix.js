ebe3606b57adee8d24b2e85ec4f8cb6cd4b65b22
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-08-26 14:19:35 +0100

    Time: 79 ms (78.62%), Space: 45.2 MB (6.38%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function compareTwoStr(str1, str2) {
+  let minLen = Math.min(str1.length, str2.length);
+  let tmp = "";
+  for (let i = 0; i < minLen; i++) {
+    if (str1[i] !== str2[i]) break;
+    tmp += str1[i];
+  }
+  return tmp;
+}
+
+var longestCommonPrefix = function (str) {
+      if (str.length === 1) return str[0];
+
+  let tail = 0;
+  let result = "";
+  for (let head = 1; head < str.length; head++) {
+    result = compareTwoStr(str[tail], str[head]);
+          if (!result) return "";
+    str[head] = result;
+
+    tail++;
+  }
+  return result;
+};
