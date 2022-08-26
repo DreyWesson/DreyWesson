4a38035a8876920329d2e7fb6f20a79c96153870
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-08-26 12:40:46 +0100

    Time: 133 ms (92.87%), Space: 48.8 MB (22.48%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function char_to_int(c) {
+  const dictionary = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  return dictionary[c];
+}
+var romanToInt = function(str1) {
+  if (!str1) return -1;
+  let num = char_to_int(str1.charAt(0));
+  let pre, curr;
+  for (var i = 1; i < str1.length; i++) {
+    curr = char_to_int(str1.charAt(i));
+    pre = char_to_int(str1.charAt(i - 1));
+    if (curr <= pre) num += curr;
+    else num = num - pre * 2 + curr;
+  }
+  return num;
+};
