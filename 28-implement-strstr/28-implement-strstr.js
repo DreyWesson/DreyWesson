21536fa455054e6b74b50151b3ac7fa4dc043460
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-08-29 11:38:13 +0100

    Time: 72 ms (78.83%), Space: 41.6 MB (93.91%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var strStr = function(str, needle) {
-     const needleLen = needle.length
-     const strLen = str.length
+     const needleLen = needle.length,
+           strLen = str.length
   if (!needleLen) return 0;
     if (needleLen > strLen) return -1;
-let tmp = ''
-  let list = -1;
+    let tmp = '',
+        list = -1;
   for (let i = 0; i < strLen; i++) {
     if (str[i] === needle[0]){
-      tmp = str.substr(i,needleLen) === needle && str.substr(i,needleLen)
+        let newVal = str.substr(i,needleLen)
+      tmp = newVal === needle && newVal
       if (tmp){
         list = i;
         break;
