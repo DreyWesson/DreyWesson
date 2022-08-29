75f45ba668e57b41e5f38128365921179cf5fdcd
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-08-29 10:33:52 +0100

    Time: 122 ms (6.92%), Space: 42.2 MB (33.13%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(str, needle) {
+     const needleLen = needle.length
+     const strLen = str.length
+  if (!needleLen) return 0;
+    if (needleLen > strLen) return -1;
+let tmp = ''
+  let list = -1;
+  for (let i = 0; i < strLen; i++) {
+    if (str[i] === needle[0]){
+      tmp = str.substr(i,needleLen) === needle && str.substr(i,needleLen)
+      if (tmp){
+        list = i;
+        break;
+      }
+    }
+  }
+  return list
+};
