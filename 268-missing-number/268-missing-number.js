d59286e4354f853b8c4e8feb0aa6d0d4df762dee
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-08-30 20:41:10 +0100

    Time: 126 ms (34.82%), Space: 47.9 MB (8.83%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,19 +3,10 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-   if (nums.length === 1) {
-        if (nums[0] > 0) return 0
-        else return 1
-    }
-    if (!nums.includes(0)) return 0;
-    
-      let setData = new Set(nums);
-  let count = 0;
-  for (let i = 1; i <= setData.size; i++) {
-    count++;
-    if (!setData.has(i)) {
-      return i;
-    }
-  }
+   if (nums.length === 1) return (nums[0] > 0)? 0 : 1;
+  if (!nums.includes(0)) return 0;
+  let setData = new Set(nums);
+  for (let i = 1; i <= setData.size; i++) 
+    if (!setData.has(i)) return i;
   return setData.size + 1;
 };
