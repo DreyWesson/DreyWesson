e8bfa9269804c381591d4903de6e805ce5b6581f
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-08-31 17:21:39 +0100

    Time: 63 ms (92.85%), Space: 42.6 MB (21.41%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,20 +4,14 @@
  * @return {number}
  */
 var searchInsert = function(array, target) {
-    let tail = 0;
-  let head = array.length - 1;
-  let mid = null;
-  if (tail > head) return false;
-  while (tail <= head) {
-    mid = ((head + tail) / 2) | 0;
-    if (array[mid] === target) return mid;
-    if (array[mid] > target) {
-      head = mid - 1
-
-    } else {
-      tail = mid + 1      
-    };
-  }
-return tail
-    
+    let head = array.length - 1,
+        tail = 0,
+        mid = null;
+    while (tail <= head) {
+        mid = ((head + tail) / 2) | 0;
+        if (array[mid] === target) return mid;
+        if (array[mid] > target) head = mid - 1;
+         else tail = mid + 1;
+    }
+    return tail
 };
