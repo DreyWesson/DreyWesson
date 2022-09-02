6d1f3103d0d563f353986c309e9ee29358c20c85
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-09-02 19:22:01 +0100

    Time: 154 ms (11.71%), Space: 51.7 MB (7.13%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,12 +4,13 @@
  */
 var isPalindrome = function(str) {
       if (str.length === 0) return true;
+    str = str.toLowerCase()
     let tmp =""
     function isAlphaNumericChar(char) {
         return char.match(/[a-zA-Z0-9]/) ? true : false ;
     }
     for (let i = 0; i < str.length; i++) 
-        if (isAlphaNumericChar(str[i])) tmp += str[i].toLowerCase();
+        if (isAlphaNumericChar(str[i])) tmp += str[i];
       let i = 0;
       let j = tmp.length - 1;
       while (i < j) {
