4e70d9fd21bef7f131ddcd37b069be8c38b8b060
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-09-03 19:23:05 +0100

    Time: 73 ms (83.92%), Space: 41.7 MB (94.24%) - LeetHub

--- 50-powx-n/50-powx-n.js
+++ 50-powx-n/50-powx-n.js
@@ -4,11 +4,5 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    // let isNeg = (n < 0)? true : false;
-    // n = Math.abs(n);
-    // let sum = 1;
-    // for(let i = 0; i < n; i++) sum *= x;
-    // if (isNeg) sum = 1 / sum;
-    // return sum.toFixed(5)
     return Math.pow(x,n)
 };
