93dc23f03a38ed4e8e8b46e02f971049bbb239dc
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-09-07 14:48:47 +0100

    Time: 102 ms (26.11%), Space: 42.3 MB (16.25%) - LeetHub

---
+++ 66-plus-one/66-plus-one.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+let remainder = 1;
+
+    for (let i = digits.length - 1; i >= 0; i--) {
+        const currentVal = digits[i];
+        digits[i] += remainder;
+        remainder = 0;
+        if (digits[i] === 10){
+            digits[i] = 0;
+            remainder = 1;
+        }
+    }
+    if (remainder !== 0) digits = [remainder, ...digits];
+    return digits;
+};
