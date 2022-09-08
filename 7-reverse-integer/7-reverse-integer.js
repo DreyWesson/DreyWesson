10fd0e7ae91ae8267fcb236ee98dfbc9c321a7b0
Dare Oduwole <50960013+DreyWesson@users.noreply.github.com>
2022-09-08 08:59:33 +0100

    Time: 118 ms (38.46%), Space: 44.2 MB (38.40%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.js
+++ 7-reverse-integer/7-reverse-integer.js
@@ -3,10 +3,18 @@
  * @return {number}
  */
 var reverse = function(x) {
-    let sign = false;
-    if (x < 0) sign = true
-    x = +String(Math.abs(x)).split('').reverse().join('');
-    if (x > 0x7FFFFFFF) 
-        return 0;
+    let sign = (x < 0) ? true : false
+    x = Math.abs(x);
+    x = Array.from(String(x))
+    let i = 0;
+    let j = x.length - 1;
+    while (i <= j){
+        [x[i], x[j]] = [x[j], x[i]]
+        
+        j--;
+        i++
+    }
+    x = Number(x.join(""))
+    if (x > 0x7FFFFFFF) return 0;
     return sign ? -x : x;
 };
